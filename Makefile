3e381f233daf3dcab959ef9c992e2bbe507f2a30
Marc Lagrange <rhaamo@gruik.at>
2011-04-13 09:50:05 +0200

    Basic Makefile and config.h

---
+++ Makefile
@@ -0,0 +1,91 @@
+# $Header: Makefile.SH,v 2.0.1.1 88/06/28 16:26:04 root Exp $
+#
+# $Log:	Makefile.SH,v $
+# Revision 2.0.1.1  88/06/28  16:26:04  root
+# patch1: support for DOSUID
+# patch1: realclean now knows about ~ extension
+#
+# Revision 2.0  88/06/05  00:07:54  root
+# Baseline version 2.0.
+#
+#
+
+CC?= cc
+bin = /usr/local/bin
+lib = /usr/local/lib/perl
+mansrc = /usr/man/man1
+manext = 1
+CFLAGS =  -O
+LDFLAGS =
+SLN = ln
+
+libs =  -lm
+
+public = perl perldb
+
+private =
+
+manpages = perl.man perldb.man
+
+util =
+
+h1 = EXTERN.h INTERN.h arg.h array.h cmd.h config.h form.h handy.h
+h2 = hash.h perl.h regexp.h spat.h stab.h str.h util.h
+h = $(h1) $(h2)
+
+c1 = arg.c array.c cmd.c dump.c eval.c form.c hash.c $(mallocsrc)
+c2 = perly.c regexp.c stab.c str.c toke.c util.c version.c
+c = $(c1) $(c2)
+
+obj1 = arg.o array.o cmd.o dump.o eval.o form.o hash.o $(mallocobj)
+obj2 = regexp.o stab.o str.o toke.o util.o version.o
+obj = $(obj1) $(obj2)
+
+lintflags = -phbvxac
+
+addedbyconf = Makefile.old bsd eunice filexp loc pdp11 usg v7
+
+.c.o:
+	$(CC) -c $(CFLAGS) $*.c
+
+all: $(public) $(private) $(util)
+	touch all
+
+perl: perly.o $(obj) perl.o
+	$(CC) $(LDFLAGS) perly.o $(obj) perl.o $(libs) -o perl
+
+
+perl.c perly.h: perl.y
+	@echo Expect 37 shift/reduce errors...
+	yacc -d perl.y
+	mv y.tab.c perl.c
+	mv y.tab.h perly.h
+
+perl.o: perl.c perly.h perl.h EXTERN.h regexp.h util.h INTERN.h handy.h config.h
+	$(CC) -c $(CFLAGS) perl.c
+
+perl.man: perl.man.1 perl.man.2
+	cat perl.man.1 perl.man.2 >perl.man
+
+install: perl perl.man
+	echo "need to be written"
+	exit
+
+clean:
+	rm -f *.o
+
+realclean:
+	rm -f perl *.orig */*.orig *~ */*~ *.o core $(addedbyconf)
+
+# The following lint has practically everything turned on.  Unfortunately,
+# you have to wade through a lot of mumbo jumbo that can't be suppressed.
+# If the source file has a /*NOSTRICT*/ somewhere, ignore the lint message
+# for that spot.
+
+lint: perl.c $(c)
+	lint $(lintflags) $(defs) perl.c $(c) > perl.fuzz
+
+test: perl
+	chmod +x t/TEST t/base.* t/comp.* t/cmd.* t/io.* t/op.*
+	cd t && (rm -f perl; $(SLN) ../perl .) && ./perl TEST
+
---
+++ config.h
@@ -0,0 +1,250 @@
+/* config.h
+ * This file was produced by running the config.h.SH script, which
+ * gets its values from config.sh, which is generally produced by
+ * running Configure.
+ *
+ * Feel free to modify any of this as the need arises.  Note, however,
+ * that running config.h.SH again will wipe out any changes you've made.
+ * For a more permanent change edit config.sh and rerun config.h.SH.
+ */
+
+
+/* EUNICE:
+ *	This symbol, if defined, indicates that the program is being compiled
+ *	under the EUNICE package under VMS.  The program will need to handle
+ *	things like files that don't go away the first time you unlink them,
+ *	due to version numbering.  It will also need to compensate for lack
+ *	of a respectable link() command.
+ */
+/* VMS:
+ *	This symbol, if defined, indicates that the program is running under
+ *	VMS.  It is currently only set in conjunction with the EUNICE symbol.
+ */
+//#undef	EUNICE		/**/
+//#undef	VMS		/**/
+
+/* CPPSTDIN:
+ *	This symbol contains the first part of the string which will invoke
+ *	the C preprocessor on the standard input and produce to standard
+ *	output.	 Typical value of "cc -E" or "/lib/cpp".
+ */
+/* CPPMINUS:
+ *	This symbol contains the second part of the string which will invoke
+ *	the C preprocessor on the standard input and produce to standard
+ *	output.  This symbol will have the value "-" if CPPSTDIN needs a minus
+ *	to specify standard input, otherwise the value is "".
+ */
+#define CPPSTDIN "/usr/bin/cpp"
+#define CPPMINUS ""
+
+/* BCOPY:
+ *	This symbol, if defined, indicates that the bcopy routine is available
+ *	to copy blocks of memory.  Otherwise you should probably use memcpy().
+ */
+#define	BCOPY		/**/
+
+/* CHARSPRINTF:
+ *	This symbol is defined if this system declares "char *sprintf()" in
+ *	stdio.h.  The trend seems to be to declare it as "int sprintf()".  It
+ *	is up to the package author to declare sprintf correctly based on the
+ *	symbol.
+ */
+//#undef	CHARSPRINTF 	/**/
+
+/* CRYPT:
+ *	This symbol, if defined, indicates that the crypt routine is available
+ *	to encrypt passwords and the like.
+ */
+//#undef	CRYPT		/**/
+
+/* DOSUID:
+ *	This symbol, if defined, indicates that the C program should
+ *	check the script that it is executing for setuid/setgid bits, and
+ *	attempt to emulate setuid/setgid on systems that have disabled
+ *	setuid #! scripts because the kernel can't do it securely.
+ *	It is up to the package designer to make sure that this emulation
+ *	is done securely.  Among other things, it should do an fstat on
+ *	the script it just opened to make sure it really is a setuid/setgid
+ *	script, it should make sure the arguments passed correspond exactly
+ *	to the argument on the #! line, and it should not trust any
+ *	subprocesses to which it must pass the filename rather than the
+ *	file descriptor of the script to be executed.
+ */
+//#undef DOSUID		/**/
+
+/* FCHMOD:
+ *	This symbol, if defined, indicates that the fchmod routine is available
+ *	to change mode of opened files.  If unavailable, use chmod().
+ */
+//#undef	FCHMOD		/**/
+
+/* FCHOWN:
+ *	This symbol, if defined, indicates that the fchown routine is available
+ *	to change ownership of opened files.  If unavailable, use chown().
+ */
+//#undef	FCHOWN		/**/
+
+/* GETGROUPS:
+ *	This symbol, if defined, indicates that the getgroups() routine is
+ *	available to get the list of process groups.  If unavailable, multiple
+ *	groups are probably not supported.
+ */
+//#undef	GETGROUPS		/**/
+
+/* index:
+ *	This preprocessor symbol is defined, along with rindex, if the system
+ *	uses the strchr and strrchr routines instead.
+ */
+/* rindex:
+ *	This preprocessor symbol is defined, along with index, if the system
+ *	uses the strchr and strrchr routines instead.
+ */
+#define	index strchr	/* cultural */
+#define	rindex strrchr	/*  differences? */
+
+/* KILLPG:
+ *	This symbol, if defined, indicates that the killpg routine is available
+ *	to kill process groups.  If unavailable, you probably should use kill
+ *	with a negative process number.
+ */
+#define	KILLPG		/**/
+
+/* MEMCPY:
+ *	This symbol, if defined, indicates that the memcpy routine is available
+ *	to copy blocks of memory.  Otherwise you should probably use bcopy().
+ *	If neither is defined, roll your own.
+ */
+#define	MEMCPY		/**/
+
+/* RENAME:
+ *	This symbol, if defined, indicates that the rename routine is available
+ *	to rename files.  Otherwise you should do the unlink(), link(), unlink()
+ *	trick.
+ */
+#define	RENAME		/**/
+
+/* SETEGID:
+ *	This symbol, if defined, indicates that the setegid routine is available
+ *	to change the effective gid of the current program.
+ */
+#define	SETEGID		/**/
+
+/* SETEUID:
+ *	This symbol, if defined, indicates that the seteuid routine is available
+ *	to change the effective uid of the current program.
+ */
+#define	SETEUID		/**/
+
+/* SETRGID:
+ *	This symbol, if defined, indicates that the setrgid routine is available
+ *	to change the real gid of the current program.
+ */
+//#undef	SETRGID		/**/
+
+/* SETRUID:
+ *	This symbol, if defined, indicates that the setruid routine is available
+ *	to change the real uid of the current program.
+ */
+//#undef	SETRUID		/**/
+
+/* STATBLOCKS:
+ *	This symbol is defined if this system has a stat structure declaring
+ *	st_blksize and st_blocks.
+ */
+//#undef	STATBLOCKS 	/**/
+
+/* STDSTDIO:
+ *	This symbol is defined if this system has a FILE structure declaring
+ *	_ptr and _cnt in stdio.h.
+ */
+//#undef	STDSTDIO 	/**/
+
+/* STRCSPN:
+ *	This symbol, if defined, indicates that the strcspn routine is available
+ *	to scan strings.
+ */
+#define	STRCSPN		/**/
+
+/* STRUCTCOPY:
+ *	This symbol, if defined, indicates that this C compiler knows how
+ *	to copy structures.  If undefined, you'll need to use a block copy
+ *	routine of some sort instead.
+ */
+#define	STRUCTCOPY	/**/
+
+/* SYMLINK:
+ *	This symbol, if defined, indicates that the symlink routine is available
+ *	to create symbolic links.
+ */
+//#undef	SYMLINK		/**/
+
+/* TMINSYS:
+ *	This symbol is defined if this system declares "struct tm" in
+ *	in <sys/time.h> rather than <time.h>.  We can't just say
+ *	-I/usr/include/sys because some systems have both time files, and
+ *	the -I trick gets the wrong one.
+ */
+//#undef	TMINSYS 	/**/
+
+/* vfork:
+ *	This symbol, if defined, remaps the vfork routine to fork if the
+ *	vfork() routine isn't supported here.
+ */
+#define	vfork fork	/**/
+
+/* VOIDSIG:
+ *	This symbol is defined if this system declares "void (*signal())()" in
+ *	signal.h.  The old way was to declare it as "int (*signal())()".  It
+ *	is up to the package author to declare things correctly based on the
+ *	symbol.
+ */
+#define	VOIDSIG 	/**/
+
+/* GIDTYPE:
+ *	This symbol has a value like gid_t, int, ushort, or whatever type is
+ *	used to declare group ids in the kernel.
+ */
+#define GIDTYPE gid_t		/**/
+
+/* STDCHAR:
+ *	This symbol is defined to be the type of char used in stdio.h.
+ *	It has the values "unsigned char" or "char".
+ */
+#define STDCHAR char	/**/
+
+/* UIDTYPE:
+ *	This symbol has a value like uid_t, int, ushort, or whatever type is
+ *	used to declare user ids in the kernel.
+ */
+#define UIDTYPE uid_t		/**/
+
+/* VOIDFLAGS:
+ *	This symbol indicates how much support of the void type is given by this
+ *	compiler.  What various bits mean:
+ *
+ *	    1 = supports declaration of void
+ *	    2 = supports arrays of pointers to functions returning void
+ *	    4 = supports comparisons between pointers to void functions and
+ *		    addresses of void functions
+ *
+ *	The package designer should define VOIDUSED to indicate the requirements
+ *	of the package.  This can be done either by #defining VOIDUSED before
+ *	including config.h, or by defining defvoidused in Myinit.U.  If the
+ *	level of void support necessary is not present, defines void to int.
+ */
+#ifndef VOIDUSED
+#define VOIDUSED 7
+#endif
+#define VOIDFLAGS 7
+#if (VOIDFLAGS & VOIDUSED) != VOIDUSED
+#define void int		/* is void to be avoided? */
+#define M_VOID		/* Xenix strikes again */
+#endif
+
+/* PRIVLIB:
+ *	This symbol contains the name of the private library for this package.
+ *	The library is private in the sense that it needn't be in anyone's
+ *	execution path, but it should be accessible by the world.
+ */
+#define PRIVLIB "/usr/local/lib/perl"		/**/
+
